18bd86c341846f261ead98f0a316cc81638bd377
jnzm246 <jackzumi909@gmail.com>
2025-09-21 12:57:24 +0700

    baitapthuchanh tuan 1 bai 5

---
+++ Baitapthuchanh_24640801_02042006/src/tuan1/bai5/Hello.java
@@ -0,0 +1,25 @@
+/**
+ * @description: baitap lab2
+ * @author: Duong Danh Dung
+ * @version 1.0
+ * @created: Sep 21, 202512:56:01 PM
+ */
+
+package tuan1.bai5;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String c;
+		c = sc.nextLine();
+		
+		System.out.println("Hello "+c);
+
+	}
+
+}
+
+
