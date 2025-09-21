82fb21943a83aeceaa75fa9e0940e467c4375c57
jnzm246 <jackzumi909@gmail.com>
2025-09-21 12:49:46 +0700

    Baitapthuchanh

---
+++ Baitapthuchanh_24640801_02042006/src/tuan1/bai4/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * @description: baitap lab2
+ * @author: Duong Danh Dung
+ * @version 1.0
+ * @created: Sep 21, 202512:34:28 PM
+ */
+
+package tuan1.bai4;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+				String a = "HelloWorld";
+				System.out.printf("HelloWorld");
+				System.out.println("\nHelloWorld");
+				System.out.printf("%s", a);
+			}
+
+
+}
+
+
